7d4a5a5d33e4b7825d86c52bcdb5d6b6eca194c4
Nicolette <nicolette.chalup@ucb.edu.bo>
2023-10-24 00:53:03 -0400

    Agregamos la funcion displayCatalogName para que nos muestre los datos por la UI.

--- src/index.js
+++ src/index.js
@@ -28,7 +28,24 @@ class CatalogList{
             catalogContainer.appendChild(catalogClone);
         });
     }
-    
+    displayCatalogName(catalogs) {
+        const catalogContainer = document.getElementById("catalog-container");
+        const catalogTemplate = document.getElementById("catalog-template");
+
+        // Limpia el contenedor de catálogos antes de mostrar nuevos resultados
+        catalogContainer.innerHTML = '';
+
+        catalogs.forEach(catalog => {
+            const catalogClone = document.importNode(catalogTemplate.content, true);
+
+            catalogClone.querySelector('.Title').textContent = catalog.Title;
+            catalogClone.querySelector('.Description').textContent = catalog.Description;
+            catalogClone.querySelector('.Difficulty').textContent = catalog.Difficulty;
+            catalogClone.querySelector('.Category').textContent = catalog.Category;
+            catalogClone.querySelector('.Type').textContent = catalog.Type;
+            catalogContainer.appendChild(catalogClone);
+        });
+    }
     searchByKataName(name) {
         // Convierte el nombre de búsqueda a minúsculas
         const searchName = name.toLowerCase();
