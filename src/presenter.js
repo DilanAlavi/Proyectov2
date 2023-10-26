62c5167e16ad3943cf7048895502e60ee7dfaaf4
Nicolette <nicolette.chalup@ucb.edu.bo>
2023-10-25 23:35:23 -0400

    COMMIT FINAL, REAFACTORIZAMOS EL PRESENTER.JS.

--- src/presenter.js
+++ src/presenter.js
@@ -1,68 +1,51 @@
-import CatalogList from './index.js'; // Importa la clase CatalogList desde el archivo correspondiente.
-import catalogData from './catalogData.js'; // Asegúrate de que la ruta sea correcta.
+import CatalogList from './index.js';
+import catalogData from './catalogData.js';
 
-// Creamos una instancia de la clase CatalogList
 const catalogListInstance = new CatalogList(catalogData);
+const catalogContainer = document.getElementById("catalog-container");
+const catalogTemplate = document.getElementById("catalog-template");
+const searchButton = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+const sortSelect = document.getElementById("sort-select");
 
-// Función para mostrar catálogos en la página
+// Función para renderizar la lista de catálogos
+function renderCatalogs(catalogs) {
+    catalogContainer.innerHTML = '';
+    catalogs.forEach(catalog => {
+        const catalogClone = document.importNode(catalogTemplate.content, true);
+        catalogClone.querySelector('.Title').textContent = catalog.Title;
+        catalogClone.querySelector('.Description').textContent = catalog.Description;
+        catalogClone.querySelector('.Difficulty').textContent = catalog.Difficulty;
+        catalogClone.querySelector('.Category').textContent = catalog.Category;
+        catalogClone.querySelector('.Type').textContent = catalog.Type;
+        catalogContainer.appendChild(catalogClone);
+    });
+}
 
-// Llamamos al método para mostrar los catálogos cuando se cargue la página
 window.onload = () => {
-   
-  catalogListInstance.renderCatalogsToContainer();
-  //"Configurando el evento de búsqueda por nombre en el botón"
-  const searchButton = document.getElementById("search-button");
-    const searchInput = document.getElementById("search-input");
-    const catalogContainer = document.getElementById("catalog-container");
-    const catalogTemplate = document.getElementById("catalog-template");
+    catalogListInstance.renderCatalogsToContainer();
 
     searchButton.addEventListener("click", () => {
         const searchName = searchInput.value.trim();
 
         if (searchName === '') {
-          // Si el campo de búsqueda está vacío, muestra un mensaje de error
-          alert("Ingrese datos para la búsqueda");
-      } else {
-          // Realiza la búsqueda normalmente
-          const searchResult = catalogListInstance.searchGlobal(searchName);
-  
-
-        // Limpia el contenido anterior en el contenedor de catálogos
-        catalogContainer.innerHTML = '';
-
-        if (searchResult.length > 0) {
-            // Agrega los nuevos resultados al contenedor
-            searchResult.forEach(catalog => {
-                const catalogClone = document.importNode(catalogTemplate.content, true);
-                catalogClone.querySelector('.Title').textContent = catalog.Title;
-                catalogClone.querySelector('.Description').textContent = catalog.Description;
-                catalogClone.querySelector('.Difficulty').textContent = catalog.Difficulty;
-                catalogClone.querySelector('.Category').textContent = catalog.Category;
-                catalogClone.querySelector('.Type').textContent = catalog.Type;
-                catalogContainer.appendChild(catalogClone);
-            });
-
-            // Actualiza el array de catálogos mostrados
-            displayedCatalogs.length = 0; // Limpia el array
-            displayedCatalogs.push(...searchResult);
+            alert("Ingrese datos para la búsqueda");
+        } else {
+            try {
+                const searchResult = catalogListInstance.searchGlobal(searchName);
+                renderCatalogs(searchResult);
+            } catch (error) {
+                alert(error.message);
+            }
         }
-         }
     });
-    
-    sortSelect.addEventListener("change", () => {
-      catalogListInstance.sortCatalogs(sortSelect.value);
 
-      // Vuelve a mostrar la lista de catálogos en el contenedor.
-      catalogContainer.innerHTML = '';
-      catalogData.forEach(catalog => {
-          const catalogClone = document.importNode(catalogTemplate.content, true);
-          catalogClone.querySelector('.Title').textContent = catalog.Title;
-          catalogClone.querySelector('.Description').textContent = catalog.Description;
-          catalogClone.querySelector('.Difficulty').textContent = catalog.Difficulty;
-          catalogClone.querySelector('.Category').textContent = catalog.Category;
-          catalogClone.querySelector('.Type').textContent = catalog.Type;
-          catalogContainer.appendChild(catalogClone);
-      });
-  });
+    sortSelect.addEventListener("change", () => {
+        const sortValue = sortSelect.value;
+
+        if (sortValue === "asc" || sortValue === "desc") {
+            catalogListInstance.OrdenarCatalogs(sortValue);
+            renderCatalogs(catalogData);
+        }
+    });
 };
- 
