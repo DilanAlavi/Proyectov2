1b5d9fcab15d97c673575ac6a436262975e844df
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2023-10-26 00:56:44 -0400

    Se Crea Katas por UI y arreglamos errores

--- src/presenter.js
+++ src/presenter.js
@@ -7,7 +7,10 @@ const catalogTemplate = document.getElementById("catalog-template");
 const searchButton = document.getElementById("search-button");
 const searchInput = document.getElementById("search-input");
 const sortSelect = document.getElementById("sort-select");
-const sortDifficultySelect = document.getElementById("sort-difficulty"); // Nuevo select
+const createKataButton = document.getElementById("create-kata-button");
+const createKataForm = document.getElementById("create-kata-form");
+const saveKataButton = document.getElementById("save-kata-button");
+const sortDifficultySelect = document.getElementById("sort-difficulty");
 
 // Función para renderizar la lista de catálogos
 function renderCatalogs(catalogs) {
@@ -49,55 +52,59 @@ window.onload = () => {
             renderCatalogs(catalogData);
         }
     });
-     // Nuevo evento para ordenar por dificultad
-     sortDifficultySelect.addEventListener("change", () => {
+
+    // Nuevo evento para ordenar por dificultad
+    sortDifficultySelect.addEventListener("change", () => {
         const sortValue = sortDifficultySelect.value;
 
         if (sortValue === "asc" || sortValue === "desc") {
             catalogData.sort((a, b) => {
                 if (sortValue === "asc") {
-                    // Ordenar de "Fácil" a "Difícil" o viceversa
-                    if (a.Difficulty < b.Difficulty) return -1;
-                    if (a.Difficulty > b.Difficulty) return 1;
-                    return 0;
+                    // Ordenar de "Fácil" a "Intermedio" a "Difícil"
+                    if (a.Difficulty === "Facil" && b.Difficulty !== "Facil") return -1;
+                    if (a.Difficulty === "Intermedio" && b.Difficulty === "Difícil") return -1;
+                    return 1;
                 } else if (sortValue === "desc") {
-                    // Ordenar de "Difícil" a "Fácil" o viceversa
-                    if (a.Difficulty > b.Difficulty) return -1;
-                    if (a.Difficulty < b.Difficulty) return 1;
-                    return 0;
+                    // Ordenar de "Difícil" a "Intermedio" a "Fácil"
+                    if (a.Difficulty === "Difícil" && b.Difficulty !== "Difícil") return -1;
+                    if (a.Difficulty === "Intermedio" && b.Difficulty === "Facil") return -1;
+                    return 1;
                 }
-
             });
 
             renderCatalogs(catalogData);
         }
-        createKataButton.addEventListener("click", () => {
-          createKataForm.style.display = "block";
-        });
     });
-     // Guardar un nuevo kata
-     saveKataButton.addEventListener("click", () => {
-      const title = document.getElementById("kata-title").value;
-      const description = document.getElementById("kata-description").value;
-      const difficulty = document.getElementById("kata-difficulty").value;
-      const category = document.getElementById("kata-category").value;
-      const type = document.getElementById("kata-type").value;
 
-      const newKata = {
-          Title: title,
-          Description: description,
-          Difficulty: difficulty,
-          Category: category,
-          Type: type
-      };
+    // Mostrar el formulario de creación de kata al hacer clic en el botón
+    createKataButton.addEventListener("click", () => {
+        catalogContainer.innerHTML = '';
+        createKataForm.style.display = "block";
+    });
 
-      // Agregar el nuevo kata al catálogo de datos (catalogData)
-      catalogData.push(newKata);
+    // Guardar un nuevo kata
+    saveKataButton.addEventListener("click", () => {
+        const title = document.getElementById("kata-title").value;
+        const description = document.getElementById("kata-description").value;
+        const difficulty = document.getElementById("kata-difficulty").value;
+        const category = document.getElementById("kata-category").value;
+        const type = document.getElementById("kata-type").value;
 
-      // Limpiar el formulario y ocultarlo
-      createKataForm.style.display = "none";
+        const newKata = {
+            Title: title,
+            Description: description,
+            Difficulty: difficulty,
+            Category: category,
+            Type: type
+        };
 
-      // Renderizar el catálogo actualizado
-      renderCatalogs(catalogData);
-  });
+        // Agregar el nuevo kata al catálogo de datos (catalogData)
+        catalogData.push(newKata);
+
+        // Limpiar el formulario y ocultarlo
+        createKataForm.style.display = "none";
+
+        // Renderizar el catálogo actualizado
+        renderCatalogs(catalogData);
+    });
 };
