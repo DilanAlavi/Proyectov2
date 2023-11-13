a1dacca813694ca8cab1a53dcd96d68fb4c9ba51
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2023-11-12 22:18:29 -0400

    Cypess, Verifica que se Edite una Kata Existente

--- cypress/e2e/Kata.cy.js
+++ cypress/e2e/Kata.cy.js
@@ -4,3 +4,15 @@ describe('Cargar Katas', () => {
       cy.get('.catalog-item').should('have.length.greaterThan', 0);
     });
   });
+  describe('Editar Kata', () => {
+    it('Verifica que se edite una Kata existente', () => {
+      cy.visit('index.html');
+      // Encuentra y hace clic en el botón de editar de la primera Kata
+      cy.get('.catalog-item').first().find('.edit-button').click();
+      // Modifica el título de la Kata
+      cy.get('#kata-title').clear().type('Kata Editada');
+      cy.get('#save-kata-button').click();
+      // Verifica que la Kata editada se muestre en el contenedor de Katas
+      cy.get('.catalog-item').contains('Kata Editada');
+    });
+  });
