7ab4c3f8e5cf45e2dd7ac04b32c04aec5c0f1b55
Valeria Rivero <valeria.rivero@ucb.edu.bo>
2023-11-14 09:34:05 -0400

    Cypress Deberia ordenar por Dificultad Descendentemente las Katas

--- cypress/e2e/Ordenar_kata.cy.js
+++ cypress/e2e/Ordenar_kata.cy.js
@@ -15,5 +15,14 @@ describe('Pruebas de Ordenamiento por titulo', () => {
         cy.get('.catalog-item:first').should('exist');
         cy.get('.catalog-item:first .Title').should('have.text', 'Ordenar Arreglos');
       });
+      describe('Pruebas de Ordenamiento por dificultad', () => {
+        it('Ordena catálogos en orden descendente', () => {
+          cy.visit("index.html")
+          cy.get('#sort-difficulty').select('desc');
+          cy.get('#sort-difficulty').should('have.value', 'desc');
+          // Espera a que el primer elemento .catalog-item esté presente
+          cy.get('.catalog-item:first').should('exist');
+          cy.get('.catalog-item:first .Title').should('have.text', 'Encontrar Ciclos en un Grafo');
+        });
   });
   
