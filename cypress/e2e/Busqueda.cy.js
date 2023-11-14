38f4f42383f7d32fd6ad9527c741c1bb9b184deb
Nicolette <nicolette.chalup@ucb.edu.bo>
2023-11-14 09:20:29 -0400

    Cypress Debe mostrar un mensaje si no hay resutlados de busqueda

--- cypress/e2e/Busqueda.cy.js
+++ cypress/e2e/Busqueda.cy.js
@@ -9,4 +9,14 @@ describe('Búsqueda Global', function() {
       cy.get('.catalog-item').should('have.length', 1);
       cy.contains('.Title', searchTerm);
     });
+    
+    it('debería mostrar un mensaje si no hay resultados de búsqueda', () => {
+        cy.visit('index.html');
+        // Intercepta la función de alerta y verifica el mensaje
+        cy.on('window:alert', (message) => {
+          expect(message).to.equal('No se encontraron resultados');
+        });
+        cy.get('#search-input').type('Término Inexistente');
+        cy.get('#search-button').click();
+      });
 });
