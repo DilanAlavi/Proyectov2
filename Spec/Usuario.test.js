877d12b7335872aa2319d36ba0cef7c8dd534f27
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2023-11-15 21:28:30 -0400

    Verifica si filtra los usuarios correctamente, devuelve un array correctamente

--- Spec/Usuario.test.js
+++ Spec/Usuario.test.js
@@ -1,7 +1,15 @@
 // Usuario.test.js
-import { crearUsuario } from "../src/Usuario";
-
-test('crearUsuario devuelve mensaje de éxito', () => {
+import { crearUsuario,FiltrarUsuarios } from "../src/Usuario";
+const mockUserData = [
+    { username: 'usuario1', password: 'clave1' },
+    { username: 'usuario2', password: 'clave2' },
+  ];
+test('Se verifica si al Registrarse devuelve mensaje de éxito', () => {
   const resultado = crearUsuario("Ejemplo Usuario", "ejemplo@dominio.com", "contrasena123");
   expect(resultado).toBe("Usuario Registrado con Exito");
 });
+test('FiltrarUsuarios debería devolver un array filtrado correctamente', () => {
+    const mockNewUser = { username: 'usuario1' };
+    const result = FiltrarUsuarios(mockUserData, mockNewUser);
+    expect(result).toEqual([{ username: 'usuario1', password: 'clave1' }]);
+  });
--- src/Usuario.js
+++ src/Usuario.js
@@ -1,3 +1,7 @@
 export function crearUsuario(nombre, correo, contraseña) {
     return "Usuario Registrado con Exito";
 }
+export function FiltrarUsuarios(userData, newUser) {
+    const { username } = newUser;
+    return userData.filter(usuario => usuario.username === username);
+}
