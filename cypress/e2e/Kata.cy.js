1057e2ec6f5afade0e3a2eb11de90a7c8487f941
Valeria Rivero <valeria.rivero@ucb.edu.bo>
2023-11-19 17:26:02 -0400

    Refactorizamos el cypress en Kata.cy.js

--- cypress/e2e/Kata.cy.js
+++ cypress/e2e/Kata.cy.js
@@ -1,45 +1,45 @@
-describe('Cargar Katas', () => {
-    it('Verifica que cargue los Datos', () => {
-      cy.visit('index.html');
-      cy.get('.catalog-item').should('have.length.greaterThan', 0);
-    });
+describe('Cargar, Editar, Crear y Eliminar Katas', () => {
+  beforeEach(() => {
+    // Asegurémonos de estar en la página principal (index.html)
+    cy.visit('index.html');
+    cy.url().should('include', 'index.html');
+
+    // Realizar el registro
+    cy.get('#crearCuentaButton').click();
+    cy.get('#nombreRegistro').type('NuevoUsuario');
+    cy.get('#correoRegistro').type('nuevo_usuario@example.com');
+    cy.get('#contrasenaRegistro').type('contrasena123');
+    cy.get('#registrarseButton').click();
+    cy.wait(1000); // Ajustar el tiempo de espera si es necesario
   });
-  describe('Editar Kata', () => {
-    it('Verifica que se edite una Kata existente', () => {
-      cy.visit('index.html');
-      // Encuentra y hace clic en el botón de editar de la primera Kata
-      cy.get('.catalog-item').first().find('.edit-button').click();
-      // Modifica el título de la Kata
-      cy.get('#kata-title').clear().type('Kata Editada');
-      cy.get('#save-kata-button').click();
-      // Verifica que la Kata editada se muestre en el contenedor de Katas
-      cy.get('.catalog-item').contains('Kata Editada');
-    });
+
+  it('Verifica que cargue los Datos de las Katas', () => {
+    cy.get('.catalog-item').should('have.length.greaterThan', 0);
   });
-  describe('Crear Nueva Kata', () => {
-    it('debería crear una nueva Kata', () => {
-      cy.visit('index.html');
-      cy.get('#create-kata-button').click();
-  
-      // Completa el formulario de creación de Kata
-      cy.get('#kata-title').type('Nueva Kata');
-      cy.get('#kata-description').type('Descripción de la nueva Kata');
-      cy.get('#kata-difficulty').select('Facil');
-      cy.get('#kata-category').type('Nueva Categoría');
-      cy.get('#kata-type').select('Ejercicio');
-      cy.get('#save-kata-button').click();
-  
-      // Verifica que la nueva Kata se muestre en el contenedor de Katas
-      cy.get('.catalog-item').contains('Nueva Kata');
-    });
+
+  it('Verifica que se edite una Kata existente', () => {
+    cy.get('.catalog-item').first().find('.edit-button').click();
+    cy.get('#kata-title').clear().type('Kata Editada').as('editedKataTitle');
+    cy.get('#save-kata-button').click();
+    
+    // Esperar a que se reflejen los cambios y luego verificar
+    cy.get('@editedKataTitle').should('exist');    
+
   });
-  describe('Eliminar Kata', () => {
-    it('debería eliminar una Kata existente', () => {
-      cy.visit('index.html');
-      // Encuentra y hace clic en el botón de eliminar de la primera Kata
-      cy.get('.catalog-item').first().find('.delete-button').click();
-  
-      // Verifica que la Kata eliminada no se muestre en el contenedor de Katas
-      cy.get('.catalog-item').should('not.contain', 'Kata Editada');
-    });
+
+  it('Debería crear una nueva Kata', () => {
+    cy.get('#create-kata-button').click();
+    cy.get('#kata-title').type('Nueva Kata');
+    cy.get('#kata-description').type('Descripción de la nueva Kata');
+    cy.get('#kata-difficulty').select('Facil');
+    cy.get('#kata-category').type('Nueva Categoría');
+    cy.get('#kata-type').select('Ejercicio');
+    cy.get('#save-kata-button').click();
+    cy.get('.catalog-item').contains('Nueva Kata');
   });
+
+  it('Debería eliminar una Kata existente', () => {
+    cy.get('.catalog-item').first().find('.delete-button').click();
+    cy.get('.catalog-item').should('not.contain', 'Kata Editada');
+  });
+});
