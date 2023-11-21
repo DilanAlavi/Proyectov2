c79b77c96f29171cb9a73d4a8f929ba7aba2453b
Nicolette <nicolette.chalup@ucb.edu.bo>
2023-11-21 09:26:28 -0400

    Deberia Añadirse el Usuario a un curso exitosamente

--- Spec/Cursos.test.js
+++ Spec/Cursos.test.js
@@ -1,4 +1,4 @@
-import { ConstructorCurso } from "../src/Cursos";
+import { ConstructorCurso,unirseCursoUsuario } from "../src/Cursos";
 
 test('Crear un objeto Curso', () => {
     const curso = new ConstructorCurso('JavaScript', 'Curso de JavaScript', 'Programación');
@@ -6,3 +6,23 @@ test('Crear un objeto Curso', () => {
     expect(curso.Description).toBe('Curso de JavaScript');
     expect(curso.Category).toBe('Programación');
   });
+// Cursos.test.js
+
+
+
+describe("unirseCursoUsuario", () => {
+  let agregarCursos;
+
+  beforeEach(() => {
+    agregarCursos = [];
+  });
+
+  test("Deberia Añadirse el Usuario a un curso exitosamente", () => {
+    const resultado = unirseCursoUsuario(agregarCursos, "JavaScript", "Curso de JavaScript", "Programación");
+    expect(resultado).toBe("Curso añadido exitosamente.");
+    expect(agregarCursos).toHaveLength(1);
+    expect(agregarCursos[0]).toEqual(ConstructorCurso("JavaScript", "Curso de JavaScript", "Programación"));
+  });
+
+
+});
--- src/Cursos.js
+++ src/Cursos.js
@@ -1,5 +1,21 @@
+// Cursos.js
+
 export function ConstructorCurso(title, description, category) {
-    this.Title = title;
-    this.Description = description;
-    this.Category = category;
-}
+    return {
+        Title: title,
+        Description: description,
+        Category: category,
+      };
+    }
+  
+    let agregarCursos = [];
+  
+    export function unirseCursoUsuario(agregarCursos, title, description, category) {
+      const nuevoCurso = ConstructorCurso(title, description, category);
+      if (!agregarCursos.some((curso) => curso.Title === nuevoCurso.Title)) {
+        agregarCursos.push(nuevoCurso);
+        return "Curso añadido exitosamente.";
+      } else {
+        return "Este curso ya ha sido agregado anteriormente.";
+      }
+ }
