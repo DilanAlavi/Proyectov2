a0ed96f94ade8c15c93ea1dbdd931b05df477f15
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2023-11-20 20:04:02 -0400

    Se crea CursosData.js y llenamos Datos

---
+++ Data/CursosData.js
@@ -0,0 +1,34 @@
+const CursosData = [
+    {
+        Title: "Curso de ATDD",
+        Description: "Curso introductorio para aprender los fundamentos de Acceptance Test-Driven Development (ATDD).",
+        Category: "Desarrollo Ágil"
+    },
+    {
+        Title: "Curso de Refactoring",
+        Description: "Curso avanzado para aprender técnicas de refactorización en el desarrollo web con React.",
+        Category: "Desarrollo Web"
+    },
+    {
+        Title: "Curso de TDD",
+        Description: "Curso sobre los principios y prácticas del Desarrollo Guiado por Pruebas (TDD) en la inteligencia artificial.",
+        Category: "Inteligencia Artificial"
+    },
+    {
+        Title: "Curso de Arquitectura de Software",
+        Description: "Curso integral sobre los principios y prácticas de la arquitectura de software. Aprende a diseñar sistemas robustos y escalables.",
+        Category: "Arquitectura de Software"
+    },
+    {
+        Title: "Curso de Desarrollo Backend con Node.js",
+        Description: "Curso centrado en el desarrollo backend con Node.js. Explora la construcción de servidores y APIs eficientes.",
+        Category: "Desarrollo Backend"
+    },
+    {
+        Title: "Curso de Seguridad en Desarrollo de Software",
+        Description: "Curso enfocado en las mejores prácticas y estrategias para garantizar la seguridad en el desarrollo de software.",
+        Category: "Seguridad en Software"
+    }
+];
+
+export default CursosData;
